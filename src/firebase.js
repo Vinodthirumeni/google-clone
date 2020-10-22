66069f934da80b118d54724f44a152e31e1a55a1
vinodthirumeni <vinodparamasivam2611@gmail.com>
2020-10-22 12:48:04 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyB_dxVV63dePJOm5VATp_qo4nkaBYEB_HI",
+  authDomain: "clone-f6873.firebaseapp.com",
+  databaseURL: "https://clone-f6873.firebaseio.com",
+  projectId: "clone-f6873",
+  storageBucket: "clone-f6873.appspot.com",
+  messagingSenderId: "362283935271",
+  appId: "1:362283935271:web:021fdeb64221cfebc8d002",
+  measurementId: "G-P6GSESQQ2K"
+};
+
+
+// If firebase expired
+// https://console.firebase.google.com/u/0/project/clone-f6873/firestore/rules
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyD4-0mWQSn4zPMa1RARP1G-cmkfOE3BC68"
+export default API_KEY
